fbd5712d2ac13dd15fde567f1dbb8ce8030b1d8b
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-12 23:08:23 +0100

    three component added

---
+++ frontend/scr/components/customers/Customers.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from 'react';
+export class Leads extends Component {
+    render() {
+        return (
+          <Fragment>
+            <h2>Leads</h2>
+            </Fragment>
+    );
+  }
+}
+export default Leads;
