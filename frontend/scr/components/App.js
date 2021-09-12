dcca8f4067b793140a34719bef738cf44d4cf0c4
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-12 23:31:23 +0100

    provider for the redux added to the app component

--- frontend/scr/components/App.js
+++ frontend/scr/components/App.js
@@ -1,14 +1,22 @@
 import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+
+
+import { Provider } from 'react-redux';
+import store from '../store';
+
 
 class App extends Component {
 
     render() {
         return (
+            <Provider store={store}>
             <Fragment>
                 <h1> First Component </h1>
             </Fragment>
-
+            </Provider>
         );
     }
 }
