212e295c02c09ad25a00c2e42b5e8c592840e826
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-13 13:01:19 +0100

    Alert component updated anytime there is an update on the message state and error , that was implemented

--- frontend/scr/components/layout/Alerts.js
+++ frontend/scr/components/layout/Alerts.js
@@ -6,16 +6,22 @@ import PropTypes from 'prop-types';
 export class Alerts extends Component {
     static propTypes = {
         error: PropTypes.object.isRequired,
+        message: PropTypes.object.isRequired,
     }
     componentDidUpdate(prevProps) {
     this.props.alert.show('It works ');
-    const { error, alert } = this.props;
+    const { error, alert ,message} = this.props;
     if (error !== prevProps.error) {
         alert.error('there is an error ');
         if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
         if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
         if (error.msg.message) alert.error(`Message: ${error.msg.message.join()}`);
     }
+    if (message !== prevProps.message) {
+        if (message.deleteCustomer) alert.success(message.deleteCustomer);
+        if (message.addCustomer) alert.success(message.addCustomer);
+        if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
+      }
     
 
     }
@@ -25,6 +31,8 @@ export class Alerts extends Component {
       }
 
   mapStateToProps = (state) => ({
-        error: state.errors});
+        error: state.errors,
+        message: state.messages,
+    });
 }
 export default connect(mapStateToProps)(withAlert()(Alerts));
