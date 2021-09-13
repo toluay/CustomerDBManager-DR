76ac38ec8c60ec82607bb3bfee7255c3c46c22b1
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-13 13:09:49 +0100

    api file updated on the customers aaplicatuon in the backend

--- Customers/api.py
+++ Customers/api.py
@@ -4,6 +4,15 @@ from .serializers import CustomerSerializer
 
 class CustomerViewSet(viewsets.ModelViewSet):
     class meta:
-        queryset = Customer.objects.all()
-        permission_classes =[permissions.AllowAny]
+        # queryset = Customer.objects.all()
+        # permission_classes =[permissions.AllowAny]
+        permission_classes = [
+        permissions.IsAuthenticated,
+    ]
         Serializer_class = CustomerSerializer
+
+        def get_queryset(self):
+            return self.request.user.leads.all()
+
+        def perform_create(self, serializer):
+            serializer.save(owner=self.request.user)
