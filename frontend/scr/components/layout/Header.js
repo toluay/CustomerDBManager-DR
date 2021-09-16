4d4c6a0462b21005901c69e55007a6617c6ecfc6
Ayantunde Tolu <ayantundetolu@gmail.com>
2021-09-16 23:14:03 +0100

    anding more effect to the header , dsplaying the users name if authenticated or display guest if not autheticated

--- frontend/scr/components/layout/Header.js
+++ frontend/scr/components/layout/Header.js
@@ -11,7 +11,33 @@ import { logout } from '../../actions/auth';
     logout: PropTypes.func.isRequired,
   };
 const { isAuthenticated, user } = this.props.auth;
+const authLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <span className="navbar-text mr-3">
+          <strong>{user ? `Welcome ${user.username}` : ''}</strong>
+        </span>
+        <li className="nav-item">
+          <button onClick={this.props.logout} className="nav-link btn btn-info btn-sm text-light">
+            Logout
+          </button>
+        </li>
+      </ul>
+    );
 
+    const guestLinks = (
+      <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+        <li className="nav-item">
+          <Link to="/register" className="nav-link">
+            Register
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </li>
+      </ul>
+    );
     render() {
         <nav className="navbar navbar-expand-sm navbar-light bg-light">
         <div className="container">
@@ -31,18 +57,7 @@ const { isAuthenticated, user } = this.props.auth;
                Customer Manager
             </a>
           </div>
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
-        <li className="nav-item">
-          <Link to="/register" className="nav-link">
-            Register
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="" className="nav-link">
-            Login
-          </Link>
-        </li>
-      </ul>
+           {isAuthenticated ? authLinks : guestLinks}
         </div>
 
       </nav>
